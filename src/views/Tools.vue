9dc8c7cdc05d6d021f878aef1f2cfe00618594f1
aixuan <415586662@qq.com>
2024-01-09 12:42:29 +0800

    add batch create wallet

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -2,8 +2,6 @@
 import ToolsJsonData from '@/data/tools';
 import { ref } from 'vue';
 import Item from '@/components/Item.vue';
-const formRef = ref<FormInstance>()
-
 let toolsData: any = ref(ToolsJsonData);
 
 </script>
