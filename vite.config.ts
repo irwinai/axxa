a22f534718ddd3a44c360b87a2bd8c7ded219282
aixuan <415586662@qq.com>
2024-01-28 00:10:02 +0800

    proxy update

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,8 @@ export default defineConfig({
     proxy: { // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
       // 正则表达式写法
       '^/api': {
-        target: 'http://localhost:8080/api', // 后端服务实际地址
+        target: 'https://api.axxa.live/', // 后端服务实际地址
+        // target: 'http://localhost:8080/',
         changeOrigin: true, //开启代理
         rewrite: (path) => path.replace(/^\/api/, '')
       }
